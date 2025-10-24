8a6ff70c36928b8fb9d1d1ade2f2c379b32617d2
User <user@Medali.local>
2025-10-24 12:50:45 +0200

    Fix Vercel base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins:[react()] })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ✅ Correction pour le chemin CSS sur Vercel
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
 })
